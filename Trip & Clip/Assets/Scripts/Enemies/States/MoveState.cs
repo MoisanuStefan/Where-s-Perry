32b5bd2218131e2d3fdd052e7bb72ff0ff746a53
StefMoisanu <34717513+MoisanuStefan@users.noreply.github.com>
2021-04-01 22:26:47 +0300

    Added basic enemy patrol
    Added a bear that patrols. On ledge or wall encounter, he idles a little, turns and continues moving.
    Class hierarchy is structured in order to easily add new enemy types, states or behaviors.

---
+++ Trip & Clip/Assets/Scripts/Enemies/States/MoveState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveState : State
+{
+    D_MoveState stateData;
+
+    protected bool isDetectingLedge;
+    protected bool isDetectingWall;
+    public MoveState(Entity entity, FiniteStateMachine stateMachine, string animBoolName, D_MoveState stateData) : base(entity, stateMachine, animBoolName)    // call base constructor, add specific constructor below
+    {
+        this.stateData = stateData;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        entity.SetVelocity(stateData.movementSpeed);
+        isDetectingLedge = entity.CheckLedge();
+        isDetectingWall = entity.CheckWall();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+        isDetectingLedge = entity.CheckLedge();
+        isDetectingWall = entity.CheckWall();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+}
